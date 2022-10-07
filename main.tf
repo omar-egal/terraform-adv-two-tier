2fb1dc6b87e5700a17dce1c7e3585420d17e4c13
EC2 Default User <ec2-user@ip-172-31-68-230.ec2.internal>
2022-10-07 14:41:59 +0000

    initial commit

---
+++ main.tf
@@ -0,0 +1,130 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.33.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = var.region
+}
+
+# Create a VPC
+resource "aws_vpc" "vpc" {
+  cidr_block = var.vpc_cidr
+
+  tags = {
+    Name = "Week-18-${var.AWS-Two-Teir-Architecture}-OmarEgal"
+  }
+}
+
+# Create VPC Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "Internet_Gateway"
+  }
+}
+
+# Create public subnets
+resource "aws_subnet" "public_subnets" {
+  count             = length(var.public_sbn_cidr_ranges)
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = element(var.public_sbn_cidr_ranges, count.index)
+  availability_zone = element(var.availability_zones, count.index)
+
+  tags = {
+    Name = "public-sbn-${count.index + 1}"
+    Tier = "Public"
+  }
+}
+
+
+# Create private subnets
+resource "aws_subnet" "private_subnets" {
+  count             = length(var.private_sbn_cidr_ranges)
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = element(var.private_sbn_cidr_ranges, count.index)
+  availability_zone = element(var.availability_zones, count.index)
+
+  tags = {
+    Name = "private-sbn-${count.index + 1}"
+    Tier = "Private"
+  }
+}
+
+# Create a route to the internet
+resource "aws_route_table" "public_route" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+# Create association for public route
+resource "aws_route_table_association" "public_sbn_association" {
+  count          = length(var.public_sbn_cidr_ranges)
+  subnet_id      = element(aws_subnet.public_subnets[*].id, count.index)
+  route_table_id = aws_route_table.public_route.id
+}
+
+# Create a security group for web access
+resource "aws_security_group" "web_sg" {
+  name        = "alb_sg"
+  description = "Allow HTTP/S inbound traffic from igw"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    description = "HTTP access"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS accessC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_inbound_web_traffic"
+  }
+}
+
+# Create Application Load Balancer
+resource "aws_lb" "alb" {
+  name               = "web-alb-tf"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.web_sg.id]
+  subnets            = [for subnet in aws_subnet.public_subnets : subnet.id]
+
+  enable_deletion_protection = true
+
+}
+
+# Create Instance targer group
+resource "aws_lb_target_group" "alb_tg" {
+  name     = "tf-web-lb-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.vpc.id
+}
